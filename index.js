829608431173017e7ccca747e5ca2be409c04002
Patricia Nykaza <pnykaza@calstatela.edu>
2019-01-23 00:09:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 
-const totalBatteries = 
+const totalBatteries = batteryBatches.reduce((total, amount) => total + amount)
 
 
 const monologueLines = [
